c1c7ac13bbeb8ce66e96ebadbe12eb7c749980e4
Julie <julie@example.com>
2023-11-06 13:16:38 +0000

    Iris

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+import os 
+from skimage.transform import resize 
+from skimage.io import imread 
+import numpy as np 
+import matplotlib.pyplot as plt 
+from sklearn import svm 
+from sklearn.model_selection import GridSearchCV 
+from sklearn.model_selection import train_test_split 
+from sklearn.metrics import accuracy_score 
+from sklearn.metrics import classification_report
